1d908495b66ab97b0355885c4bd593066b20eb2c
Ayomide Onalaja <ayo.onalaja@gmail.com>
2025-12-03 01:07:15 +0100

    api/issues/POST

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,17 +1,17 @@
 import Link from 'next/link';
-import { FaBug } from 'react-icons/fa';
+import { IoBugOutline } from 'react-icons/io5';
 import { MdOutlineSpaceDashboard } from 'react-icons/md';
 import { AiOutlineIssuesClose } from 'react-icons/ai';
 
 export default function NavBar() {
   const links = [
-    { label: <FaBug size={26} />, href: '/' },
+    { label: <IoBugOutline size={26} />, href: '/' },
     { label: <MdOutlineSpaceDashboard size={26} />, href: '/dashboard' },
     { label: <AiOutlineIssuesClose size={26} />, href: '/issues' },
   ];
 
   return (
-    <nav className='h-dvh w-20 flex flex-col gap-6 items-center bg-amber-700 py-4 px-2'>
+    <nav className='h-dvh w-20 flex flex-col gap-6 items-center justify-center bg-amber-700 py-4 px-2'>
       {links.map((link) => (
         <Link key={link.href} href={link.href}>
           {link.label}
