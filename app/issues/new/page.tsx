c37ab6c1d90e8be0e0186c39e58c5afa9ca4e081
Ayomide Onalaja <ayo.onalaja@gmail.com>
2025-12-04 01:06:45 +0100

    add wsigw editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,13 +1,17 @@
+'use client';
 import { Box, Button, TextArea, TextField } from '@radix-ui/themes';
+import SimpleMDE from 'react-simplemde-editor';
+import 'easymde/dist/easymde.min.css';
 
 export default function NewIssue() {
   return (
     <div className='flex flex-col gap-4'>
-      <Box maxWidth='400px'>
+      <Box maxWidth='500px'>
         <TextField.Root size='2' placeholder='Issue Title' />
       </Box>
-      <Box maxWidth='400px'>
-        <TextArea size='2' placeholder='Issue Description' />
+      <Box maxWidth='500px'>
+        {/* <TextArea size='2' placeholder='Issue Description' /> */}
+        <SimpleMDE />
       </Box>
       <Box>
         <Button variant='classic' radius='large' color='orange'>
